b97c9f708fc69c71bf41ddb92e0d66529c8807bb
MichaelMitch <mitchellwyne@gmail.com>
2023-04-01 15:14:17 -0700

    weatherpy finished

--- Starter_Code/starter_code/api_keys.py
+++ Starter_Code/starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "b9c28e38f9b9f03c92bce4f8047ad1d9"
+weather_api_key = "7e7ed452c09ea20825afdb52fe21b8a5"
 
 # Geoapify API Key
 geoapify_key = "792caf747d314aafa905e6b195fcc12e"
